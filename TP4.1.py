9e420adf3463b5ed5e580a8f2f029e977e56433f
Cédric <soheib.boudali29@gmail.com>
2019-11-17 21:31:53 +0100

    add function to extract date and money

--- TP4.1.py
+++ TP4.1.py
@@ -2,12 +2,25 @@ def Load(file_name):
 	file= open(file_name,"r")
 	return file.read()
 
-def PuncTokenizer(text , reg_exp):
+def PuncTokenizer(text):
+	from re import VERBOSE
+	regex_punct=r"""(?x)
+ 	[^\w\s]+  # poctuation 
+	""" 
 	from nltk.tokenize import RegexpTokenizer
-	tokenizer =  RegexpTokenizer(reg_exp)
+	tokenizer =  RegexpTokenizer(regex_punct)
 	return tokenizer.tokenize(text)
-from re import VERBOSE
-regex_punct=r"""(?x)
- [^\w\s]+  # poctuation 
-""" 
-print(PuncTokenizer(Load("Corpus.txt"), regex_punct))
+print(PuncTokenizer(Load("Corpus.txt")))
+
+def Tokenizer(text):
+	from  nltk.tokenize import RegexpTokenizer
+	from re import VERBOSE
+	reg_ex=r"""(?x)
+	\$\d+(?:\.\d+)? #sommes monétaires en dollar
+	|\£\d+(?:\.\d+)? #sommes monétaires en euro
+	|\d{1,2}.\d{1,2}.\d{4} #date format dd X mm X yyyy month format numeric
+	|\d{1,2}.(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).\d{4} #date format dd X mm X yyyy month format string
+	"""
+	tokenizer = RegexpTokenizer(reg_ex)
+	return tokenizer.tokenize(text)
+print(Tokenizer(Load("Corpus.txt")))
