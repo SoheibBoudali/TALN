2d193a2b07412374845daadafea484ba47b6ff0c
Cédric <soheib.boudali29@gmail.com>
2019-11-17 17:33:34 +0100

    adding a punctiation tokenizer using  verbose

--- TP4.1.py
+++ TP4.1.py
@@ -1,5 +1,13 @@
 def Load(file_name):
 	file= open(file_name,"r")
 	return file.read()
-text=Load("Corpus.txt")
-print(text)
+
+def PuncTokenizer(text , reg_exp):
+	from nltk.tokenize import RegexpTokenizer
+	tokenizer =  RegexpTokenizer(reg_exp)
+	return tokenizer.tokenize(text)
+from re import VERBOSE
+regex_punct=r"""(?x)
+ [^\w\s]+  # poctuation 
+""" 
+print(PuncTokenizer(Load("Corpus.txt"), regex_punct))
