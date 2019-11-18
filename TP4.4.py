411919ae62ab1d6b7ce09281ca0a3c2b4ae607d1
Cédric <soheib.boudali29@gmail.com>
2019-11-18 01:55:36 +0100

    optimisation of the code in comment

---
+++ TP4.4.py
@@ -0,0 +1,15 @@
+words = ['attribution', 'confabulation', 'elocution', 'sequoia','tenacious', 'unidirectional']
+vsequences=set([''.join([char for char in word if char in 'aeiou']) for word in words])
+print(sorted(vsequences))
+'''
+words = [’attribution’, ’confabulation’, ’elocution’, ’sequoia’,’tenacious’, ’unidirectional’]
+vsequences = set()
+for word in words:
+	vowels = []
+ 	for char in word:
+ 		if char in ’aeiou’:
+ 			vowels.append(char)
+ 	vsequences.add(’’.join(vowels))
+sorted(vsequences)
+[’aiuio’, ’eaiou’, ’eouio’, ’euoia’, ’oauaio’, ’uiieioa’]
+'''
