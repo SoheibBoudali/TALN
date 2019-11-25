3f623d2270c4f9512d8f9da93b4e809f9ebabc3a
Cédric <soheib.boudali29@gmail.com>
2019-11-25 12:57:48 +0100

    code review TP5.3

--- TP5.3.py
+++ TP5.3.py
@@ -4,9 +4,13 @@ from nltk import bigrams
 from operator import itemgetter
 
 words= RegexpTokenizer(r'\w+').tokenize(open("Corpus.txt","r",encoding='utf-8').read())
-stop_words= open("stopwords_fr.txt","r",encoding='utf-8').read().split()
-words=[word for word in words if word not in stop_words]
+stop_words= open("stopwords_en.txt","r",encoding='utf-8').read().split()
+#words=[word for word in words if word not in stop_words]
 bigrams= bigrams(words)
-freqdist= FreqDist(bigrams)
+liste=[]
+for big in bigrams:
+	if big[1] not in stop_words and big[0] not in stop_words:
+		liste.append(big)
+freqdist= FreqDist(liste)
 for item in sorted(freqdist.items(), key=itemgetter(1), reverse=True)[:10]:
-    print(item)
+	print(item)
--- TP5.4.py
+++ TP5.4.py
@@ -1,5 +1,6 @@
 from nltk import ConditionalFreqDist
 from nltk.corpus import brown
+import matplotlib
 words=['The', 'Fulton', 'County', 'Grand', 'Jury', 'said']
 CondFreqDist=ConditionalFreqDist((categorie,word) for categorie in brown.categories() for word in brown.words(categories=categorie))
 CondFreqDist.tabulate(samples=words)
